b7a07f377af49bc8beee735eb496995616952774
Hungdz2024 <166615236+Hungdz2024@users.noreply.github.com>
2026-09-19 09:09:41 +0700

    Add files via upload

---
+++ HelloFunction.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+void inDuongKe() {
+    cout << "========================================" << endl;
+}
+void chaoMung() {
+    cout << "Xin chao! Chao mung ban den voi C++." << endl;
+}
+int main() {
+    inDuongKe();
+    chaoMung();
+    inDuongKe();
+    return 0;
+}
